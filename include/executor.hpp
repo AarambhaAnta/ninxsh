94f5a58cc11114ee9a0fdd27a8add7b2b94be8d1
AarambhaAnta <aarambha108@gmail.com>
2025-06-13 23:26:40 +0530

    feat: Add signal handler and zombie cleanup function declarations

--- include/executor.hpp
+++ include/executor.hpp
@@ -5,5 +5,7 @@
 
 void executeExternal(const ParsedCommand& cmd);
 void executePipeline(const ParsedCommand& cmd);
+void setupSignalHandlers();
+void cleanupZombieProcesses();
 
 #endif  // EXECUTOR_HPP
